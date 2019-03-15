097c6fa4b476f047fa3514eca2c0c8330530613f
Darya K <47159287+dkweller@users.noreply.github.com>
2019-03-15 19:11:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,74 @@ public class Main {
         //end loop
 
         //write to file
+        
+         String input = "";
+        int in = 0;
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while(in != 4) {
+
+            while (in > 4 || in < 1) {
+
+                System.out.print("What shape would you like? " +
+                        "\n 1 : Triangle " +
+                        "\n 2 : Rectangle " +
+                        "\n 3 : Diamond " +
+                        "\n 4 : EXIT PROGRAM\n\n");
+
+                input = scanner.next();
+        
+    
+                try {
+                    in = Integer.parseInt(input);
+                } catch (NumberFormatException ex) {
+                    in = 0;
+                }
+
+                if(in == 4) {
+                    System.exit(0);
+                }
+
+            }
+
+            while (n > 100 || n < 1) {
+
+                System.out.print("\nPlease input a height be1tween 1 and 100: ");
+
+                if (scanner.hasNextInt()) {
+                    n = scanner.nextInt();
+                }
+
+            }
+
+            System.out.println(" ");
+
+            switch (in) {
+                case 1:
+                    Triangle x = new Triangle();
+                    x.printShape(n);
+                    break;
+                case 2:
+                    Rectangle t = new Rectangle();
+                    t.printShape(n);
+                    break;
+                case 3:
+                    Diamond s = new Diamond();
+                    s.printShape(n);
+                    break;
+                default:
+                    // blank
+                    break;
+            }
+
+            System.out.println(" ");
+
+            //resets values
+            input = "0";
+            in = 0;
+            n = 0;
+
     }
 
 }
