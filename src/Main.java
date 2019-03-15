5c756ee6624e837a1f4be648a1b81f44da9350d8
username006 <ceckhardt06@gmail.com>
2019-03-15 15:48:28 -0700

    comments added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
 
+    enum SHAPE{
+        TRIANGLE,
+        RECTANGLE,
+        DIAMOND
+    }
+
     public static void main(String[] args) {
         prompt();
     }
 
     private static void prompt() {
 
-        
+        //create shape
 
+        //loop start
+        //get user input
+
+        //check user input
+
+        //call printshape()
+
+        //end loop
+
+        //write to file
     }
 
 }
--- src/Shapes.java
+++ src/Shapes.java
@@ -1,4 +1,15 @@
 public class Shapes {
 
+    //data structure to store
+
+    //constructor
+
+    //printShape(enum, int)
+
+    //Diamond(int) prints rectangle of size n and adds to array
+
+    //Rectangle(int)  prints diamond of size n and adds to array
+
+    //triangle(int) prints triangle of size n and adds to array
 
 }
