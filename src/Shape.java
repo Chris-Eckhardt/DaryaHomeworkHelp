34bb32c4c5c6d6f70dae2b6ff48caec525e3d52a
Darya K <47159287+dkweller@users.noreply.github.com>
2019-03-15 19:09:14 -0400

    Update Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -18,11 +18,36 @@ public class Shape {
 
     //Diamond(int) prints rectangle of size n and adds to array
     private String diamond(int n) { return null; }
+    
+    for(int i = 0; i < x; i++){
+            for(int j = x- i; j > 0; j--) {
+                 System.out.print(" ");
+            }
+            
+                for(int j = 0; j < x; j++) {
+           System.out.print("* "); 
+            }
+            System.out.println("");
+    
 
     //Rectangle(int) prints diamond of size n and adds to array
     private String rectangle(int n) { return null; }
+    
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < x; j++) {
+           System.out.print("* "); 
+            }
+            System.out.println("");
 
     //triangle(int) prints triangle of size n and adds to array
     private String triangle(int n) { return null; }
-
+           System.out.println("");
+        for(int i = 1; i <= n; i++) {
+            System.out.println("");
+            for(int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+        }
+        System.out.println("");
+           
 }
